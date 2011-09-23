15fa323aee9e23c93f605f777054fee52070a203
Frederic Thibodeau <thibodeau.frederic.2@courrier.uqam.ca>
2011-09-23 11:08:57 -0400

    test with tortoisegit

--- Sources/ca/uqam/inf2015/aut2011/parmenide/Principale.java
+++ Sources/ca/uqam/inf2015/aut2011/parmenide/Principale.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2011, UQAM
- * 
+ *
  * $Revision: 1.0.0.0 $
  */
 package ca.uqam.inf2015.aut2011.parmenide;
@@ -9,42 +9,42 @@ import ca.uqam.inf2015.aut2011.parmenide.infrastructure.log.Logger;
 
 /**
  * Classe principale d entree du projet
- * 
+ *
  * @author Frederic Thibodeau
  * @author Roosvelt Lamour
- * @version 1.0
+ * @version 1.1
  */
 public class Principale {
 
     private static CalculateRepayLoan calculateRepayLoan;
-    
+
     /**
-     * 
+     *
      * L entree principale du program
-     * 
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         try {
             calculateRepayLoan = new CalculateRepayLoan(args);
             validateInputs();
-            calculateLoanWithInputs();            
-        
+            calculateLoanWithInputs();
+
         }catch(Exception ex){
-            Logger.Log(ex.getMessage());            
+            Logger.Log(ex.getMessage());
         }
-        
-        Logger.Log("Process Done"); 
+
+        Logger.Log("Process Done");
     }
-    
+
     private static void validateInputs() throws Exception {
         if(!calculateRepayLoan.isValidArgsInputs()){
             throw new Exception("Bad arguments passed");
         }
     }
-    
+
     private static void calculateLoanWithInputs(){
         calculateRepayLoan.calculateLoanWithInputs();
     }
-   
+
 }
